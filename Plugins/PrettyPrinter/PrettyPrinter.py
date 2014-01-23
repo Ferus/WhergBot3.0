eea690b93c5fe42ef0c6ca34341b5bef28f5b0e4
Kevin Li <nerd_rage@live.com>
2014-01-23 18:59:20 -0500

    Fix PrettyPrinter plugin
    ffs

--- Plugins/PrettyPrinter/PrettyPrinter.py
+++ Plugins/PrettyPrinter/PrettyPrinter.py
@@ -16,4 +16,10 @@ class Plugin(object):
 		return True
 
 	def call(self, message):
+		# Refuck text to print
+		if len(message.params) == 2:
+			message.params.append(" ".join(message.params.pop(1)))
 		print("\033[31m[Bot: {0}] \033[32m{1}\033[0m".format(self.bot.name, repr(message)))
+		# Unfuck text before it's refucked
+		if len(message.params) == 2:
+			message.params.append(message.params.pop(1).split(" "))
